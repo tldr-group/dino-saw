262b0403e1f5b92fd4844589247542a1093318df
Moritz Pawlowsky <moritz.hans.dieter.pawlowsky@datascience.uni-giessen.de>
2025-11-12 14:13:00 +0100

    adding import for GenericDataset

--- dinosaw/datasets/__init__.py
+++ dinosaw/datasets/__init__.py
@@ -2,8 +2,9 @@ __all__ = [
     "generate_teacher_dataset",
     "DatasetTrainStudent",
     "DatasetValStudent",
-    "TeacherDataset"
+    "TeacherDataset",
+    "GenericDatasetStudent",
 ]
 from .generating_teacher_embeddings import generate_teacher_dataset
-from .train_student_dataset import DatasetTrainStudent, DatasetValStudent
+from .train_student_dataset import DatasetTrainStudent, DatasetValStudent, GenericDatasetStudent
 from .teacher_dataset import TeacherDataset
