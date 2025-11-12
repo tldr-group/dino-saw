7c0cb17f4660a357b84406ac64c41fab8eb6387e
Moritz Pawlowsky <moritz.hans.dieter.pawlowsky@datascience.uni-giessen.de>
2025-11-12 14:11:48 +0100

    usage of GenericDataset

--- dinosaw/trainer.py
+++ dinosaw/trainer.py
@@ -2,12 +2,12 @@ from lightning import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
 import torch
 from dinosaw.models import PEModel
-from dinosaw.datasets import DatasetTrainStudent, DatasetValStudent
+from dinosaw.datasets import DatasetTrainStudent, DatasetValStudent, GenericDatasetStudent
 
 
 def main():
-    train_loader = torch.utils.data.DataLoader(DatasetTrainStudent(), batch_size=32, num_workers=56, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(DatasetValStudent(), batch_size=2)
+    train_loader = torch.utils.data.DataLoader(GenericDatasetStudent(base_path="/home/ab_aimd_anja_20884/Pawlowsky_Moritz/England/DINOMO/testing_dinov2/Dataset/IN_reduced_224", split="train"), batch_size=32, num_workers=0, pin_memory=True, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(GenericDatasetStudent(base_path="/home/ab_aimd_anja_20884/Pawlowsky_Moritz/England/DINOMO/testing_dinov2/Dataset/IN_reduced_224", split="val"), batch_size=32, num_workers=8, pin_memory=True)
 
     checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
@@ -16,7 +16,7 @@ def main():
         mode="min"
     )
     
-    trainer = Trainer(devices=1, max_epochs=70, gradient_clip_val=1.0, callbacks=checkpoint_callback) #, strategy="ddp"
+    trainer = Trainer(devices=1, max_epochs=20, gradient_clip_val=1.0, callbacks=checkpoint_callback) #, strategy="ddp"
     trainer.fit(model=PEModel(), train_dataloaders=train_loader, val_dataloaders=val_loader)
 
 
