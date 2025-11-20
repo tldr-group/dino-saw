1bdb42cb64291981219096b49eb7d2433dc8d92c
Moritz Pawlowsky <moritz.hans.dieter.pawlowsky@datascience.uni-giessen.de>
2025-11-20 16:29:39 +0100

    actually used AliBi

--- dinosaw/trainer.py
+++ dinosaw/trainer.py
@@ -6,18 +6,18 @@ from dinosaw.datasets import DatasetTrainStudent, DatasetValStudent, GenericData
 
 
 def main():
-    train_loader = torch.utils.data.DataLoader(GenericDatasetStudent(base_path="/home/ab_aimd_anja_20884/Pawlowsky_Moritz/England/DINOMO/testing_dinov2/Dataset/IN_reduced_224", split="train"), batch_size=32, num_workers=0, pin_memory=True, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(GenericDatasetStudent(base_path="/home/ab_aimd_anja_20884/Pawlowsky_Moritz/England/DINOMO/testing_dinov2/Dataset/IN_reduced_224", split="train"), batch_size=32, num_workers=8, pin_memory=True, shuffle=True)
     val_loader = torch.utils.data.DataLoader(GenericDatasetStudent(base_path="/home/ab_aimd_anja_20884/Pawlowsky_Moritz/England/DINOMO/testing_dinov2/Dataset/IN_reduced_224", split="val"), batch_size=32, num_workers=8, pin_memory=True)
 
     checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
         dirpath='trained_models/',
-        filename='test-{epoch:02d}-{val_loss:.2f}',
+        filename='normalised_alibi-{epoch:02d}-{val_loss:.2f}',
         mode="min"
     )
     
-    trainer = Trainer(devices=1, max_epochs=70, gradient_clip_val=1.0, callbacks=checkpoint_callback) #, strategy="ddp"
-    trainer.fit(model=PEModel(), train_dataloaders=train_loader, val_dataloaders=val_loader)
+    trainer = Trainer(devices=1, max_epochs=60, gradient_clip_val=1.0, callbacks=checkpoint_callback) #, strategy="ddp"
+    trainer.fit(model=PEModel(use_alibi=True), train_dataloaders=train_loader, val_dataloaders=val_loader)
 
 
 if __name__ == "__main__":
