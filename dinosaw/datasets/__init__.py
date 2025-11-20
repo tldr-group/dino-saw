0cf1852be7aaf436f8a46bb0be327d8025215d35
Moritz Pawlowsky <moritz.hans.dieter.pawlowsky@datascience.uni-giessen.de>
2025-11-20 16:29:09 +0100

    fixed imports

--- dinosaw/datasets/__init__.py
+++ dinosaw/datasets/__init__.py
@@ -1,10 +1,6 @@
 __all__ = [
-    "generate_teacher_dataset",
     "DatasetTrainStudent",
     "DatasetValStudent",
-    "TeacherDataset",
     "GenericDatasetStudent",
 ]
-from .generating_teacher_embeddings import generate_teacher_dataset
 from .train_student_dataset import DatasetTrainStudent, DatasetValStudent, GenericDatasetStudent
-from .teacher_dataset import TeacherDataset
