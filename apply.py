2e91c14fa187842b7c39f73d7b01d393068237d6
rmdocherty <rmdocherty2@gmail.com>
2026-01-26 18:35:32 +0000

    update plots; add mic notebook

--- apply.py
+++ apply.py
@@ -15,30 +15,34 @@ model = AlibiVitWrapper(
     wrap=True,
 )
 
-weights = torch.load("trained_models/alibi_dv2_vits14_reg.pth", weights_only=True, map_location="cuda:0")
+weights = torch.load("trained_models/alibi_homog_dv2_vits14_reg.pth", weights_only=True, map_location="cuda:0")
 model.load_state_dict(weights)
+# model.half()
 model.eval()
 
 # model = PretrainedViTWrapper(MODEL_LIST[1], stride=4, add_flash_attn=False, device="cuda:0")
 
-img_fname = "snowcat.png"
-_img = Image.open(f"images/{img_fname}").convert("RGB")
-_img = _img.resize((512, 512))
+img_fname = "NMC_2D_less_wide_crop.png"
+_img = Image.open(f"images/micro/{img_fname}").convert("RGB")
+# _img = _img.resize((512, 512))
 
-
-tr = closest_resize(_img.height, _img.width, 14)
-img, _ = load_image(f"images/{img_fname}", tr, to_half=False, device_str="cuda:0")
+SF = 1
+tr = closest_resize(SF * _img.height, SF * _img.width, 14)
+img, _ = load_image(f"images/micro/{img_fname}", tr, to_half=False, device_str="cuda:0")
 with torch.no_grad():
     emb = model.forward_features(img, make_2D=True)
 
 emb_np = to_numpy(emb.squeeze(0))
 print(emb_np.shape)
 
+channels_to_blank = [47, 113, 117, 359]
+emb_np[channels_to_blank, :, :] = 0
+
 pca_emb = do_2D_pca(emb_np, n_components=3, post_norm="minmax")
 print(pca_emb.shape)
-plt.imsave("tmp/emb_518.png", pca_emb)
+plt.imsave("tmp/emb_518_.png", pca_emb)
 
 
-alibi_matrix = model.model.blocks[0].attn.distance_matrix.cpu().numpy()
-print(alibi_matrix.shape)
-plt.imsave("tmp/alibi_518.png", alibi_matrix, cmap="hot")
+# alibi_matrix = model.model.blocks[0].attn.distance_matrix.cpu().numpy()
+# print(alibi_matrix.shape)
+# plt.imsave("tmp/alibi_518.png", alibi_matrix, cmap="hot")
