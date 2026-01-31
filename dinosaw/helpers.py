dd54993e044d58c43977ca0d3e4674c08fe1d187
rmdocherty <rmdocherty2@gmail.com>
2026-01-31 20:53:41 +0000

    adde extra models

--- dinosaw/helpers.py
+++ dinosaw/helpers.py
@@ -12,7 +12,24 @@ import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
 
-ModelTypes = Literal["dv2", "dv2_b", "dvt", "alibi_dv2", "alibi_dv2_h", "alibi_dv2_cb", "nope"]
+ModelTypes = Literal[
+    "dv2",
+    "dv2_b",
+    "dvt",
+    "alibi_dv2",
+    "alibi_dv2_h",
+    "alibi_dv2_cb",
+    "nope",
+    "dv",
+    "dv3",
+    "vit_b",
+    "clip_b",
+    "eva02_b",
+    "sam_b",
+    "convnext",
+]
+TimmModels = Literal["dv2", "dv2_b", "dv", "dv3", "vit_b", "clip_b", "eva02_b", "sam_b", "convnext"]
+timm_models = get_args(TimmModels)
 model_types: tuple[ModelTypes] = get_args(ModelTypes)
 model_names: dict[ModelTypes, str] = {
     "dv2": "DINOv2",
@@ -22,6 +39,12 @@ model_names: dict[ModelTypes, str] = {
     "alibi_dv2_h": "ALiBi(H)-Dv2",
     "alibi_dv2_cb": "ALiBi(CB)-Dv2",
     "nope": "NoPE",
+    "dv": "DINO",
+    "dv3": "DINOv3",
+    "clip_b": "CLIP-B",
+    "eva02_b": "EVA02-B",
+    "sam_b": "SAM-B",
+    "convnext": "",
 }
 model_chkpoints: dict[ModelTypes, str] = {
     "dv2": "",
@@ -31,20 +54,44 @@ model_chkpoints: dict[ModelTypes, str] = {
     "alibi_dv2_h": "alibi_homog_dv2_vits14_reg.pth",
     "alibi_dv2_cb": "alibi_cb_dv2_vits14_reg.pth",
     "nope": "nope_dv2_vits14_reg.pth",
+    "dv": "",
+    "dv3": "dinov3_vits_patch16_plus_reg4.pth",
+    "vit_b": "",
+    "clip_b": "",
+    "eva02_b": "",
+    "sam_b": "",
+    "convnext": "",
+}
+model_name_to_timm: dict[TimmModels, str] = {
+    "dv2": MODEL_LIST[1],
+    "dv2_b": MODEL_LIST[3],
+    "dv": MODEL_LIST[5],
+    "dv3": MODEL_LIST[4],
+    "vit_b": MODEL_LIST[6],
+    "clip_b": MODEL_LIST[7],
+    "eva02_b": MODEL_LIST[8],
+    "sam_b": MODEL_LIST[9],
+    "convnext": MODEL_LIST[10],
 }
 
 
-def get_model(model_type: ModelTypes, model_dir: str, device: str, to_half: bool = False) -> PretrainedViTWrapper:
+def get_model(
+    model_type: ModelTypes, model_dir: str, device: str, to_half: bool = False, conf_path: str | None = None
+) -> PretrainedViTWrapper:
     S = 14
     model: PretrainedViTWrapper | None = None
 
-    if model_type in ("dv2", "dv2_b"):
-        model_id = MODEL_LIST[1] if model_type == "dv2" else MODEL_LIST[3]
+    if model_type in timm_models:
+        model_id = model_name_to_timm[model_type]
+        model_chk = model_chkpoints[model_type] if model_type == "dv3" else None
+        S = 16 if "patch16" in model_id else 14
         model = PretrainedViTWrapper(
             model_id,
             stride=S,
             add_flash_attn=False,
             device=device,
+            chk_path=model_chk,
+            conf_path=conf_path,
         )
         model = model.eval()
         if to_half:
@@ -104,6 +151,7 @@ def get_features(
     channel_blank: bool = False,
     to_half: bool = False,
     device: str = "cuda:0",
+    S: int = 14,
 ) -> np.ndarray:
     tr = closest_resize(pil_img.height, pil_img.width, model.stride)
     img_tensor = convert_image(pil_img, tr, device_str=device, to_half=to_half)
